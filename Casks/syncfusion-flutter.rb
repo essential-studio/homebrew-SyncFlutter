6e6a931fd6aa27c4f04f328188ef93faf7aaa992
christopherhirudayasamy <93127137+christopherhirudayasamy@users.noreply.github.com>
2025-02-13 16:41:27 +0530

    Update syncfusion-flutter.rb

--- Casks/syncfusion-flutter.rb
+++ Casks/syncfusion-flutter.rb
@@ -1,16 +1,16 @@
-cask "your-app" do
+cask "syncfusion-flutter" do
   version "28.2.3"
   sha256 "0b7475b60eaa5a4c1048010a1643fa20e12b1e02b9c0898e00bf1087703c5adb"
 
   url "https://github.com/essential-studio/homebrew-SyncFlutter/releases/download/v28.2.3/syncfusionessentialflutter_trial.dmg"
-  name "Your App"
+  name "Syncfusion Flutter"
   desc "Flutter installer for MAC and Linux"
   homepage "https://Syncfusion.com"
 
-  app "YourApp.app"
+  artifact "Syncfusion", target: "~/Applications/Syncfusion"
 
   zap trash: [
-    "~/Library/Application Support/YourApp",
-    "~/Library/Preferences/com.yourcompany.yourapp.plist",
+    "~/Library/Application Support/Syncfusion",
+    "~/Library/Preferences/com.syncfusion.flutter.plist",
   ]
 end
