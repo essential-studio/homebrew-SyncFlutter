7ba9bcc6ccfa70a41edf35e2f92f1f45da70583e
Kalaiyarasu <kalaiyarasu.sf4149@gmail.com>
2025-02-28 13:00:30 +0530

    Shasum value updated

--- Casks/MAUI/syncfusion-maui@28.1.33.rb
+++ Casks/MAUI/syncfusion-maui@28.1.33.rb
@@ -1,6 +1,6 @@
 cask "syncfusion-maui@28.1.33" do
   version "28.1.33"
-  sha256 "0b7475b60eaa5a4c1048010a1643fa20e12b1e02b9c0898e00bf1087703c5adb"
+  sha256 "b70b3ab7d9ba7917ba234fe8757d0d4a51ea052b882660841c0a530869ba82ee"
 
   url "https://files2.syncfusion.com/Installs/v28.1.33/Mac/syncfusionessentialmaui_trial.dmg"
   name "Syncfusion MAUI"
